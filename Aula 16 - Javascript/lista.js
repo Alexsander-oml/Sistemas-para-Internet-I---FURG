b15948bd0ee96710cf5c45873a45e5ef184ba30d
Alexsander Santos <61764134+Alexsander-oml@users.noreply.github.com>
2024-11-05 13:57:59 +0000

    função para cálculo da média e exibição de resultado

---
+++ Aula 16 - Javascript/lista.js
@@ -0,0 +1,13 @@
+function media() {
+    n1 = parseFloat(document.getElementById("n1").value);
+    n2 = parseFloat(document.getElementById("n2").value);
+    n3 = parseFloat(document.getElementById("n3").value);
+    n4 = parseFloat(document.getElementById("n4").value);
+    media = (n1 + n2 + n3 + n4) / 4;
+
+    if (M > 7) {
+        alert("Aprovado");
+    } else {
+        alert("Reprovado");
+    }
+}
